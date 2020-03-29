390aa999c2037cb29c454d5d88fa7fcd53143d0f
Vishal Yadav <52189323+Vishalyadav0804@users.noreply.github.com>
2020-03-29 07:34:11 +0530

    Add files via upload

--- model/User.js
+++ model/User.js
@@ -1,1 +1,28 @@
+const mongoose=require('mongoose');
 
+const userSchema=new mongoose.Schema({
+    name: {
+        type:String,
+        min:6,
+        max:255,
+        required:true
+    },
+    email: {
+        type:String,
+        max:255,
+        required:true,
+        min:6
+    },
+    password: {
+        type:String,
+        required:true,
+        max:1024,
+        min:6
+    },
+    date: {
+        type:Date,
+        default:Date.now
+    }
+});
+
+module.exports=mongoose.model('User',userSchema);
